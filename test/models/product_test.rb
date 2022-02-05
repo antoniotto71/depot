e341eeb3056f02745e6ea6d3dc72ba43d8cb0fcb
antoniotto71 <antoniotto71@yahoo.it>
2022-02-05 19:46:09 +0100

    Test caching

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,7 +12,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title: "My Book Title", description: "whatever you like", image_url: "aaa.jpg")
+    product = Product.new(title: "My Book Title",
+                          description: "whatever you like",
+                          image_url: "aaa.jpg")
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
